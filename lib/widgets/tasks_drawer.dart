be0277282787e35db19269d3b6d98196e241e446
Desiree Armojallas <desiree.armojallas.20@usjr.edu.ph>
2022-08-06 11:49:56 +0800

    Made changes to main.dart

--- lib/widgets/tasks_drawer.dart
+++ lib/widgets/tasks_drawer.dart
@@ -1,65 +1,47 @@
+import 'package:bloc_finals_exam/blocs/tasks/bloc/tasks_bloc.dart';
+import 'package:bloc_finals_exam/blocs/theme/bloc/theme_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
-import '../screens/recycle_bin_screen.dart';
-import '../screens/tabs_screen.dart';
-import '../test_data.dart';
+import 'app_router.dart';
+import 'app_themes.dart';
+import 'screens/tabs_screen.dart';
 
-class TasksDrawer extends StatelessWidget {
-  const TasksDrawer({Key? key}) : super(key: key);
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final storage = await HydratedStorage.build(
+      storageDirectory: await getApplicationDocumentsDirectory());
 
-  _switchToDarkTheme(BuildContext context, bool isDarkTheme) {
-    if (isDarkTheme) {
-    } else {}
-  }
+  HydratedBlocOverrides.runZoned(() => runApp(MyApp(appRouter: AppRouter())),
+      storage: storage);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key, required this.appRouter}) : super(key: key);
+
+  final AppRouter appRouter;
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Drawer(
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 20),
-              child: Text(
-                'Task Drawer',
-                style: Theme.of(context).textTheme.headline5,
-              ),
-            ),
-            ListTile(
-              leading: const Icon(Icons.folder_special),
-              title: const Text('My Tasks'),
-              trailing: Text(
-                '${TestData.pendingTasks.length} | ${TestData.completedTasks.length}',
-              ),
-              onTap: () => Navigator.pushReplacementNamed(
-                context,
-                TabsScreen.path,
-              ),
-            ),
-            const Divider(),
-            ListTile(
-              leading: const Icon(Icons.delete),
-              title: const Text('Recycle Bin'),
-              trailing: Text('${TestData.removedTasks.length}'),
-              onTap: () => Navigator.pushReplacementNamed(
-                context,
-                RecycleBinScreen.path,
-              ),
-            ),
-            const Divider(),
-            const Expanded(child: SizedBox()),
-            ListTile(
-              leading: Switch(
-                value: TestData.isDarkTheme,
-                onChanged: (newValue) => _switchToDarkTheme(context, newValue),
-              ),
-              title: const Text('Switch to Dark Theme'),
-              onTap: () => _switchToDarkTheme(context, !TestData.isDarkTheme),
-            ),
-            const SizedBox(height: 10),
-          ],
-        ),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => TasksBloc()),
+        BlocProvider(create: (context) => ThemeBloc())
+      ],
+      child: BlocBuilder<ThemeBloc, ThemeState>(
+        builder: (context, state) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'BloC Tasks App',
+            theme: state.isDarkTheme
+                ? AppThemes.appThemeData[AppTheme.darkMode]
+                : AppThemes.appThemeData[AppTheme.lightMode],
+            home: const TabsScreen(),
+            onGenerateRoute: appRouter.onGenerateRoute,
+          );
+        },
       ),
     );
   }
